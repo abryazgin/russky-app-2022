9fa8e1cffc9ac2c7d6ea31d6f0baa216191b6aee
abryazgin <abryazgin@yandex-team.ru>
2022-04-11 01:20:23 +0500

    apm agent works

--- Makefile
+++ Makefile
@@ -4,6 +4,15 @@ BIN = venv/bin/
 CODE = russky
 SRC = .
 TEST = tests
+OTEL_ENVS = ""
+
+add_local_env_file:
+    # https://www.elastic.co/guide/en/apm/get-started/current/open-telemetry-elastic.html
+	echo EOF > .env
+	export OTEL_RESOURCE_ATTRIBUTES=service.name=russky-app-2022,service.version=1.0,deployment.environment=dev
+	export OTEL_EXPORTER_OTLP_ENDPOINT=https://localhost:8200
+	#export OTEL_EXPORTER_OTLP_HEADERS="Authorization=Bearer an_apm_secret_token"
+	EOF
 
 test:
 	PYTHONPATH=$(SRC) $(BIN)pytest --verbosity=2 --showlocals --strict-markers $(TEST)
--- russky/app.py
+++ russky/app.py
@@ -1,3 +1,6 @@
+import logging
+
+from elasticapm.contrib.starlette import make_apm_client, ElasticAPM
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -9,15 +12,20 @@ from russky.models import RecommendationResponse
 from russky.settings import DataSourceSetting, LoggingSetting
 
 app = FastAPI()
+apm = make_apm_client()
+app.add_middleware(ElasticAPM, client=apm)
 LoggingSetting().setup()
 data_sources = MulitpleDataSources(DataSourceSetting())
 templates = Jinja2Templates(directory='templates')
 
 app.mount('/static', StaticFiles(directory='static'), name='static')
 
+logger = logging.getLogger(__name__)
+
 
 @app.get('/', response_class=HTMLResponse)
 async def read_item(request: Request) -> _TemplateResponse:
+    logger.info("get random recommendation log entry")
     return templates.TemplateResponse(
         'recommendations/index.html',
         {'request': request, 'recommendation': data_sources.get_random_recommendation()},
