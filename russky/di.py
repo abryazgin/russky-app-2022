62a07c9bdeb29ed3090f398bdbe930fa261cf8b6
SAwckA <46552699+SAwckA@users.noreply.github.com>
2022-05-06 02:08:10 +1000

    Update di.py

--- russky/di.py
+++ russky/di.py
@@ -3,7 +3,7 @@ from starlette.templating import Jinja2Templates
 from russky.data_sources import MulitpleDataSources
 from russky.settings import DataSourceSetting
 
-
+#DI
 class DI:
     data_sources = MulitpleDataSources(DataSourceSetting())
     templates = Jinja2Templates(directory='templates')
